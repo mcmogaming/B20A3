b55d1f6dfc56855920db5c2a22b07fed50998b71
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 01:35:03 -0400

    blach

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,25 @@
-import sqlite3
+import database as d
+import re
 import hashlib
-from flask import Flask, render_template, request, g, redirect, url_for
+import random
+import string
+from flask import Flask, render_template, request, g, redirect, url_for, make_response
+
 
 SESSIONS = dict() #contains sessiond_id and userid
-DATABASE = './database.db'
-
-#check login credentials
-def check_login(username, password):
-    con = sqlite3.connect('database2.db')
-    cur = con.cursor()
-    cur.execute('SELECT * FROM login_credentials')
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1] == username and row[2] == hashlib.md5(password.encode()).hexdigest():
-            return True
-    return False
-
-#The function get_db gets the databse
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+PRIVILEGES = {"Guest":0,"Student":1,"Teaching Assistant":2,"Professor":3,"Admin":4,}
 
 #Creating Flask Instance
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+def check_session():
+    sessionId = request.cookies.get('sessionId')
+    if not (sessionId in SESSIONS.keys()):
+        print('FAILED Session Check')
+        return True
+    else:
+        print('PASSED Session Check')
+        return False
 
 #Check Login
 @app.route('/')
@@ -45,16 +34,157 @@ def root():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        print("Attempted Login With:" + request.form['username'] + request.form['password'])
+        print('Attempted Login With:' + request.form['username'] + request.form['password'])
         return do_the_login(request.form['username'],request.form['password'])
     else:
         return show_login_page()
 
 def do_the_login(username,password):
     if check_login(username,password):
-        return "Login Success"
+        if 'sessionId' in request.cookies:
+            print('Pass')
+        else:
+            print('Fail')    
+        sessionId = newSessionID()
+        userquery = d.query_assoc('SELECT * FROM login_credentials WHERE username = '' + username + ''')[0]
+        user = User(userquery)
+        SESSIONS[sessionId] = user
+        resp = make_response(redirect('/dashboard'))
+        resp.set_cookie('sessionId', sessionId, max_age=60*30)
+        return resp
     return show_login_page(login_failed = True)
 
 def show_login_page(login_failed=False):
-    #return "Login Page"
+    #return 'Login Page'
+    print("showing login page")
     return render_template('login.html', login_failed=login_failed)
+
+def check_login(username, password):
+    rows = d.query('SELECT * FROM login_credentials')
+    for row in rows:
+        if row[1] == username and row[2] == hashlib.md5(password.encode()).hexdigest():
+            return True
+    return False    
+
+#Register Page
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        return do_register()
+    else:
+        return show_register_page()
+
+def do_register():
+    r = request
+    userid = r.form['userid']    
+    username = r.form['username']
+    password = r.form['password']
+    password_r = r.form['password_r']
+    fullname = r.form['fullname']
+    courses = r.form['courses']
+    privilege = r.form['privilege']
+
+
+    #check inputed values
+    sql = "SELECT * FROM 'main'.'login_credentials' WHERE 'username' = ? OR 'userid' = ? "
+    num_entries = d.num_entries(sql, (username, userid))
+    if num_entries != 0:
+        print("Username/UserID already exists")
+        g.loginexists = True
+        return show_register_page()
+    if password != password_r:
+        print("Password Don't Match")
+        g.passwordnotmatch = True
+        return show_register_page()
+    if not re.match('^[a-zA-Z0-9]*$',username):
+        print("Username not Alphanumeric")       
+        g.invalid_username = True
+        return show_register_page()
+    if not re.match('^[a-zA-Z0-9]*$',password):
+        print("Password not Alphanumeric")       
+        g.invalid_password = True
+        return show_register_page()
+    # if not re.match('^[a-zA-Z]*$',fullname):
+    #     g.invalid_fullname = True
+    #     show_register_page()    
+    if not re.match('^[0-9]*$',userid):
+        print("userid not numeric")         
+        g.invalid_userid = True
+        return show_register_page()
+    #post them in database
+    sql = "INSERT INTO login_credentials (userid, username, hashedpwd, name, privilege, courses) VALUES (?, ?, ?, ?, ?, ?)"
+    privilege = PRIVILEGES[privilege]
+    d.query_t(sql,(userid, username, hashlib.md5(password.encode()).hexdigest(), fullname, privilege, courses))
+    g.accountcreated = True
+    print("Account Created Successfully")
+    return show_login_page()
+
+def show_register_page():
+    return render_template('register.html')
+
+
+
+#Dashboard Page
+@app.route('/dashboard')
+def show_dashboard():
+    if check_session():
+        return redirect('/login')
+    else:
+        user = SESSIONS.get(request.cookies.get('sessionId'))
+        #setup profile
+        g.username = user.username
+        g.name = user.name
+        g.courses = user.courses
+        g.privilege = user.privilege_title
+
+        #setup courses
+        return render_template('dashboard.html')
+
+#Home Page
+@app.route('/home')
+def show_homepage():
+    if check_session():
+        return redirect('/login')
+    else:
+        user = SESSIONS.get(request.cookies.get('sessionId'))
+        #setup profile
+        g.username = user.username
+        g.name = user.name
+        g.courses = user.courses
+        g.privilege = user.privilege_title
+
+        #setup courses
+        return render_template('home.html')
+
+#session
+def newSessionID():
+    print('Generating a new SessionID')
+    letters = string.ascii_letters
+    sessionId = ''.join(random.choice(letters) for i in range(256))
+    print(sessionId)
+    return sessionId
+
+def getSessionId():
+    return request.cookies.get('sessionId')
+
+#classes
+class User:
+    def __init__(self, sqlquery):
+        self.userid = sqlquery['userid']
+        self.username = sqlquery['username']
+        self.name = sqlquery['name']
+        self.courses = str(sqlquery['courses']).split(',')
+        self.privilege = sqlquery['privilege']
+        if self.privilege  == 0:
+            self.privilege_title = 'Guest'
+        elif self.privilege  == 1:
+            self.privilege_title = 'Student'
+        elif self.privilege  == 2:
+            self.privilege_title = 'Teaching Assistant'
+        elif self.privilege  == 3:
+            self.privilege_title = 'Professor'
+        elif self.privilege  == 4:
+            self.privilege_title = 'Admin'
+        else:
+            self.privilege_title = 'Unknown'
+        
--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,47 @@
 import sqlite3
 
-con = sqlite3.connect('database2.db')
-cur = con.cursor()
+#This module will contain all functions required for a database
 
-cur.execute('SELECT * FROM login_credentials')
-row = cur.fetchall()
-print(row)
-print(len(row))
-print(row[1])
+DATABASE_PATH = "database.db"
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
-#print all rows in login_credentials
+def query(sql):
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+def query_t(sql, values):
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(sql, values)
+    rows = cur.fetchall()
+    con.commit()
+    con.close()
+    return rows
+
+#creates a list of dicts where the key in each dict is the column
+def query_assoc(sql):
+    con = sqlite3.connect(DATABASE_PATH)
+    con.row_factory = dict_factory 
+    cur = con.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+def num_entries(sql, values=None):
+    con = sqlite3.connect(DATABASE_PATH)
+    con.row_factory = dict_factory 
+    cur = con.cursor()
+    cur.execute(sql, values)
+    rows = cur.fetchall()
+    con.close()
+    return len(rows)
